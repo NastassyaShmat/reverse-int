e36deed19f90334f1ce1426f8feb17c6f9f2b3a8
NastassyaShmat <79085022+NastassyaShmat@users.noreply.github.com>
2021-02-27 18:42:30 +0300

    feat: added full functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    let reversed = String(n).split('').reverse().join(''); 
+    let result = Number(reversed);
+    if (n < 0) {
+        n = Math.abs(n);
+        reversed = String(n).split('').reverse().join(''); 
+        result = Number(reversed);
+
+    }
+    return result;
+ }
